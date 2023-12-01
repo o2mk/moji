63ec181230268cc03d3563cf9687879469f729fe
桃香 福田 <momoka_moka@icloud.com>
2023-12-01 10:30:13 +0900

    return 0 ok!

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main() {
-	const int col = 8;
-	const int row = 8;
 
-	FILE* fp = fopen("data.txt", "r");
+	const int col = 8; //８桁
+	const int row = 8; //８桁
 
-	char data[row][col + 1] = {};
+	FILE* fp = fopen("data.txt", "r"); //ファイルオープン
+
+	char data[row][col + 1] = {}; //  char型
 	int r = 0;
-	while (fscanf(fp, "%s", data[r]) != EOF) {
+	while (fscanf(fp, "%s", data[r]) != EOF){
 		r++;
-	}
+	} //r++をすることで読み取れる
 
-	fclose(fp);
+	fclose(fp); //ファイルクローズ
 
-	for (int r = 0; r < row; r++) {
-		for (int c = 0; c < col; c++) {
-			if (data[r][c] == '1') {
-				printf("*");
+	for (int r = 0; r < row; r++) { //row
+		for (int c = 0; c < col; c++) { //col
+			if (data[r][c] == '1') { //もしそのデータが'1'だった場合 -1に''を入れる!!!-
+				printf("*"); //全角"＊"表示
 			}
-			else {
-				printf("　");
+			else { //じゃなかったら
+				printf("　"); //全角スペースを表示する
 			}
 		}
-		printf("\n");
+		printf("\n"); //改行する
 	}
 
-	return 0;
+	return 0; //問題なしなら0!!!
 }
