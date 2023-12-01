8aa0bd9d121adf0b0f01a10a2074d61d46fda58f
桃香 福田 <momoka_moka@icloud.com>
2023-12-01 10:18:54 +0900

    add #

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	const int col = 8;
+	const int row = 8;
+
+	FILE* fp = fopen("data.txt", "r");
+
+	char data[row][col + 1] = {};
+	int r = 0;
+	while (fscanf(fp, "%s", data[r]) != EOF) {
+		r++;
+	}
+
+	fclose(fp);
+
+	for (int r = 0; r < row; r++) {
+		for (int c = 0; c < col; c++) {
+			if (data[r][c] == '1') {
+				printf("*");
+			}
+			else {
+				printf("@");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
